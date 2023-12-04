a197e769e2a007c6c9a81028177c681101198768
sp-andrew-gaskell <andrew.gaskell@socialpoint.es>
2023-12-04 17:30:04 +0100

    add day4 part 2

--- day04/main.go
+++ day04/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"slices"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -18,22 +19,47 @@ func caculateTotalPoints(input string) {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	totalPoints := 0
+	copies := map[int]int{}
 	for scanner.Scan() {
-		points := calculateLinePoints(scanner.Text())
+		id, points, matches := calculateLine(scanner.Text())
 		totalPoints += points
+
+		copies[id]++
+
+		for i := id + 1; i <= id+matches; i++ {
+			copies[i] += copies[id]
+		}
+	}
+
+	totalCards := 0
+	for _, v := range copies {
+		totalCards += v
 	}
 	fmt.Println("Total points:", totalPoints)
+	fmt.Println("Total cards:", totalCards)
 }
 
-func calculateLinePoints(line string) int {
+func parseCardId(cardId string) int {
+	parts := strings.Fields(cardId)
+	id, err := strconv.Atoi(parts[1])
+	if err != nil {
+		panic(err)
+	}
+	return id
+}
+
+func calculateLine(line string) (int, int, int) {
 	tmp := strings.Split(line, ": ")
-	tmp = strings.Split(tmp[1], " | ")
-	winningNums := strings.Fields(tmp[0])
-	myNums := strings.Fields(tmp[1])
+	id := parseCardId(tmp[0])
+	nums := strings.Split(tmp[1], " | ")
+	winningNums := strings.Fields(nums[0])
+	myNums := strings.Fields(nums[1])
 
 	points := 0
+	matches := 0
 	for _, n := range myNums {
 		if slices.Contains(winningNums, n) {
+			matches++
 			if points == 0 {
 				points = 1
 			} else {
@@ -41,7 +67,7 @@ func calculateLinePoints(line string) int {
 			}
 		}
 	}
-	return points
+	return id, points, matches
 }
 
 func main() {
