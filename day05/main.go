c0ff69d87027b5d77626285b20594a5d2b621c7b
sp-andrew-gaskell <andrew.gaskell@socialpoint.es>
2023-12-05 17:54:09 +0100

    day 5 part2

--- day05/main.go
+++ day05/main.go
@@ -11,7 +11,8 @@ import (
 )
 
 type (
-	mapValues [][][3]int
+	mappings   [][][3]int
+	seedRanges [][2]int
 )
 
 func parseSeeds(line string) (seeds []int) {
@@ -23,7 +24,14 @@ func parseSeeds(line string) (seeds []int) {
 	return
 }
 
-func parseMap(mapValues mapValues, line string) mapValues {
+func parseSeedRanges(seeds []int) (seedRanges seedRanges) {
+	for i := 0; i < len(seeds)-1; i += 2 {
+		seedRanges = append(seedRanges, [2]int{seeds[i], seeds[i+1]})
+	}
+	return
+}
+
+func buildMapFromLine(mapValues mappings, line string) mappings {
 	if strings.Contains(line, "map:") {
 		mapValues = append(mapValues, [][3]int{})
 	} else if line == "" {
@@ -41,19 +49,65 @@ func parseMap(mapValues mapValues, line string) mapValues {
 	return mapValues
 }
 
-func findLocations(seeds []int, maps mapValues) (locations []int) {
-	for _, value := range seeds {
-		for _, mapValues := range maps {
-			for _, values := range mapValues {
-				if value >= values[1] && value < values[1]+values[2] {
-					value = value - values[1] + values[0]
+func findLocationsByRange(seedRanges seedRanges, mappings mappings) []int {
+	var locations []int
+	for _, sdRange := range seedRanges {
+		ranges := [][2]int{{sdRange[0], sdRange[0] + sdRange[1]}}
+		for _, mapping := range mappings {
+			var transformed [][2]int
+			for _, mapFilter := range mapping {
+				dst, src, srcEnd := mapFilter[0], mapFilter[1], mapFilter[1]+mapFilter[2]
+				var notTransformed [][2]int
+				for _, r := range ranges {
+					before := [2]int{r[0], min(r[1], src)}
+					inter := [2]int{max(r[0], src), min(srcEnd, r[1])}
+					after := [2]int{max(srcEnd, r[0]), r[1]}
+
+					if before[0] < before[1] {
+						notTransformed = append(notTransformed, before)
+					}
+					if inter[0] < inter[1] {
+						transformedRange := [2]int{inter[0] - src + dst, inter[1] - src + dst}
+						transformed = append(transformed, transformedRange)
+					}
+					if after[0] < after[1] {
+						notTransformed = append(notTransformed, after)
+					}
+				}
+				ranges = notTransformed
+			}
+			ranges = append(ranges, transformed...)
+		}
+		minLocation := findMinLocationRanges(ranges)
+		locations = append(locations, minLocation)
+	}
+	return locations
+}
+
+func findLocations(seeds []int, mappings mappings) (locations []int) {
+	for _, seedVal := range seeds {
+		for _, mapping := range mappings {
+			for _, mapFilter := range mapping {
+				dst, src, sz := mapFilter[0], mapFilter[1], mapFilter[2]
+				if seedVal >= src && seedVal < src+sz {
+					seedVal = seedVal - src + dst
 					break
 				}
 			}
 		}
-		locations = append(locations, value)
+		locations = append(locations, seedVal)
 	}
 	return
+}
+
+func findMinLocationRanges(locations [][2]int) int {
+	min := locations[0][0]
+	for _, l := range locations {
+		if l[0] < min {
+			min = l[0]
+		}
+	}
+	return min
 }
 
 func findMinLocation(locations []int) int {
@@ -66,6 +120,20 @@ func findMinLocation(locations []int) int {
 	return min
 }
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func solve(input string) {
 	file, err := os.Open(input)
 	if err != nil {
@@ -75,17 +143,22 @@ func solve(input string) {
 	scanner := bufio.NewScanner(file)
 
 	seeds := []int{}
-	mapValues := mapValues{}
+	mappings := mappings{}
 	for scanner.Scan() {
 		if len(seeds) == 0 {
 			seeds = parseSeeds(scanner.Text())
 		} else {
-			mapValues = parseMap(mapValues, scanner.Text())
+			mappings = buildMapFromLine(mappings, scanner.Text())
 		}
 	}
-	locations := findLocations(seeds, mapValues)
+	locations := findLocations(seeds, mappings)
 	minLocation := findMinLocation(locations)
 	fmt.Println("Part 1 - Min location:", minLocation)
+
+	seedRanges := parseSeedRanges(seeds)
+	locations = findLocationsByRange(seedRanges, mappings)
+	minLocation = findMinLocation(locations)
+	fmt.Println("Part 2 - Min location:", minLocation)
 }
 
 func main() {
